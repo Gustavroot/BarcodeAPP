1ac2fcf5322518ecfec4b5766ef2292e3bd69569
Gustavo Ramirez Hidalgo <tavroot@gmail.com>
2013-08-07 14:39:36 -0600

    camara

--- BarcodeAPP/app/view/MyContainer.js
+++ BarcodeAPP/app/view/MyContainer.js
@@ -31,9 +31,10 @@ Ext.define('MyApp.view.MyContainer', {
                 handler: function(button, event) {
                     try{
                         navigator.camera.getPicture(onSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.DATA_URL});
-                        function onSuccess(imageData) {
+                        function 	onSuccess(imageData) {
                             var image = document.getElementById('myImage');
                             image.src = "data:image/jpeg;base64," + imageData;
+                            alert("Exitoo....")
                         }
                         function onFail(message) {
                             alert('Failed because: ' + message);
